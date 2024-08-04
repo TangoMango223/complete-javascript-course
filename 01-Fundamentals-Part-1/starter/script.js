2618a2b98b9169b2101af05f6d8f982c21be9ac1
TangoMango223 <tang.c.christine@gmail.com>
2024-08-04 17:20:03 -0400

    added code

---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,8 @@
+// This is inline script, only advantage is that you don't have to load other file. But it gets messy, you should separate JS stuff.--> //
+<script>
+    let js = "amazing";
+    if (js == "amazing") alert("Hi Christine!");
+    console.log(40 + 10 - 10);
+</script>
+
+// You need console.log in order 
