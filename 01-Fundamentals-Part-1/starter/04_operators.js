e0d3e06cf9e64127920198a5e330066daf775f72
TangoMango223 <tang.c.christine@gmail.com>
2024-08-04 20:55:40 -0400

    added code for script 4

--- 01-Fundamentals-Part-1/starter/04_operators.js
+++ 01-Fundamentals-Part-1/starter/04_operators.js
@@ -1,10 +1,15 @@
 // Let's look at Math Operators
 
 // Example #1 - Christine's age in 2024
-const ageChristine = 27;
+const ageChristine = 2037 - 1997;
 console.log(ageChristine);
 
 
 // Age of Sarah - she's old haha!
-const ageSarah = 70;
+const ageSarah = 2037 - 2018;
 console.log(ageSarah);
+
+// Multiple things to do:
+console.log(ageChristine, ageSarah);
+
+// By default we are using Constants or CONST
