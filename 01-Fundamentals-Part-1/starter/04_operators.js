02071e88caa55134d7e111b9ef8cf9210991c418
TangoMango223 <tang.c.christine@gmail.com>
2024-08-04 20:59:15 -0400

    added script 5 for math operators

--- 01-Fundamentals-Part-1/starter/04_operators.js
+++ 01-Fundamentals-Part-1/starter/04_operators.js
@@ -13,3 +13,36 @@ console.log(ageSarah);
 console.log(ageChristine, ageSarah);
 
 // By default we are using Constants or CONST
+
+// We can log multiple values at once.
+console.log(ageChristine * 2, (ageChristine / 10) ** 2)
+
+// Similar to Python, 2 ** 3  = 8 
+// PEDMAS also applies here
+
+
+// Example with strings
+const firstName = "Christine"
+const lastName = "Tang"
+
+// Full name
+console.log(firstName + lastName);
+
+// Add a space to separate it out, yay!
+console.log(firstName + " " + lastName);
+
+// Template strings, like F-String, for better ways of doing this in the future
+
+// Type of operator:
+console.log(typeof firstName);
+
+// Assignment Operator
+// Use equal sign
+// Will equal 15
+let x = 10 + 5;
+
+
+// Console log
+console.log(x);
+
+//
