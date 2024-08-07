d51542ce4c63555fe5fb57967cdecbe4d2274471
Christine Tang <tang.c.christine@gmail.com>
2024-08-07 16:19:40 -0400

    added note 5

--- 02-Fundamentals-Part-2/starter/05_other_funcs.js
+++ 02-Fundamentals-Part-2/starter/05_other_funcs.js
@@ -2,9 +2,31 @@
 
 // Example:
 function cutFruitPieces(fruit){
-    return fruit *4;
+    return fruit * 4;
 }
 
+
+// // Initial function, which will only give you the whole fruit:
+// function fruitProcessor(apples, oranges){
+//  const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
+//  const applePieces = cutFruitPieces(apples);
+//  const orangePieces = cutFruitPieces(oranges);
+//  return juice;   
+// }
+
+console.log(fruitProcessor(2,3));
+
+// Modify to call the cut pieces
 function fruitProcessor(apples, oranges){
-    
-}
+
+    // Pieces:
+    const applePieces = cutFruitPieces(apples);
+    const orangePieces = cutFruitPieces(oranges);
+
+    // Juice composition
+    const juice = `Juice with ${applePieces} apples and ${orangePieces} oranges.`;
+
+    return juice;   
+   }
+   
+   console.log(fruitProcessor(2,3));
