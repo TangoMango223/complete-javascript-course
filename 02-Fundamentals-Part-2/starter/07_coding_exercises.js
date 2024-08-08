549ff4263f2f33e93cdc7858220a3fcd743bdc9d
Christine Tang <tang.c.christine@gmail.com>
2024-08-07 23:22:42 -0400

    sync notes

--- 02-Fundamentals-Part-2/starter/07_coding_exercises.js
+++ 02-Fundamentals-Part-2/starter/07_coding_exercises.js
@@ -37,25 +37,70 @@
 // ------------------------------------ 
 // Coding Challenge #2 - Steven's Tip calculator
 
-function calcTip(bill) {
-    if (bill <= 300 && bill >= 50){
-        return 0.15 * bill;
-    } else {
-        return 0.2 * bill;
+// function calcTip(bill) {
+//     if (bill <= 300 && bill >= 50){
+//         return 0.15 * bill;
+//     } else {
+//         return 0.2 * bill;
+//     }
+// }
+
+// // Create arrays
+// let bills, tips;
+
+
+// // Define bill
+// bills = [125, 555, 44];
+
+// // Define tip
+// tips = [calcTip(bills[0]), calcTip(bills[1]),calcTip(bills[2])];
+// console.log(tips);
+
+// // BONUS Challenge:
+// const totals = [bills[0] + tips[0],bills[1] + tips[1],bills[2] + tips[2]];
+// console.log(totals);
+
+// ------------------------------------ 
+// Coding Challenge #3 - BMI Calculator again
+
+const mark = {
+    fullName: "Mark Miller",
+    mass: 78,
+    height: 1.69,
+    calcBMI: function() {
+        this.bmi = this.mass / (this.height * this.height);
+        return this.bmi;
     }
 }
 
-// Create arrays
-let bills, tips;
+const john = {
+    fullName: "John Smith",
+    mass: 92,
+    height: 1.95,
+    calcBMI: function() {
+        this.bmi = this.mass / (this.height * this.height);
+        return this.bmi;
+    }
+}
+
+// Calculate BMI
+// john.calcBMI();
+// mark.calcBMI();
 
 
-// Define bill
-bills = [125, 555, 44];
+// Log into the console:
+// console.log(`${mark.calcBMI() < john.calcBMI() ?`John Smith's BMI (${john.bmi}) is higher than Mark Miller's (${mark.bmi})!`: `Mark Miller's (${mark.bmi}) is higher than John Smith's BMI (${john.bmi})`});
 
-// Define tip
-tips = [calcTip(bills[0]), calcTip(bills[1]),calcTip(bills[2])];
-console.log(tips);
+// if (mark.bmi > john.bmi) {
+//     console.log(`${mark.fullName}'s BMI (${mark.bmi}) is higher than ${john.fullName}'s (${john.bmi})!`)
+//   } else if (john.bmi > mark.bmi) {
+//     console.log(`${john.fullName}'s BMI (${john.bmi}) is higher than ${mark.fullName}'s (${mark.bmi})!`)
+//   }
 
-// BONUS Challenge:
-const totals = [bills[0] + tips[0],bills[1] + tips[1],bills[2] + tips[2]];
-console.log(totals);
+
+  // Christine's better solution that calls each function 
+  // that defines properties and uses ternary operators.
+  console.log(`${mark.calcBMI() < john.calcBMI() ? 
+    `John Smith's BMI (${john.bmi}) is higher than Mark Miller's (${mark.bmi})!` : 
+    `Mark Miller's BMI (${mark.bmi}) is higher than John Smith's BMI (${john.bmi})!`}`);
+    
---
+++ 02-Fundamentals-Part-2/starter/13_loops.js
@@ -0,0 +1,26 @@
+// Loops and Iterations in JS
+
+// Important for automation and following DRY sometimes
+// This is shit code LOL
+// console.log('Lifting weights, repetition 1 🤸‍♀️');
+// console.log('Lifting weights, repetition 1 🤸‍♀️');
+// console.log('Lifting weights, repetition 1 🤸‍♀️');
+// console.log('Lifting weights, repetition 1 🤸‍♀️');
+// console.log('Lifting weights, repetition 1 🤸‍♀️');
+// console.log('Lifting weights, repetition 1 🤸‍♀️');
+
+
+// For Loops in JavaScript - Syntax
+// Set loop stop condition - will happen until no longer TRUE
+// Syntax: for (var initialization; set loop stop condition; rep = rep + 1)
+for(let rep = 1; rep <= 30; rep++){
+    console.log(`Lifting weights, repetition ${rep} 🤸‍♀️`);
+}
+
+// Dev tools shows that the print has happened 10 times!
+
+for (initialization; condition; increment) 
+    {
+        // code to be executed 
+        }
+    
