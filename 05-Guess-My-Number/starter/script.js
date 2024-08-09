00ef8e68e815bcc105663429afa2bf37d5fd3c18
TangoMango223 <tang.c.christine@gmail.com>
2024-08-09 16:00:37 -0400

    added some game logic

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -12,7 +12,7 @@ console.log(document.querySelector('.message')); // first DOM
 console.log(document.querySelector('.message').textContent); // The text is "start guessing..." YAY!
 
 // We will select the message and change text to Connect number.
-document.querySelector('.message').textContent = "Correct Number!"
+// document.querySelector('.message').textContent = "Correct Number!"
 
 // Console check - we see it's been permanently changed to Correct Number.
 // If page is reloaded or changed in another way, it will revert away.
@@ -21,17 +21,17 @@ document.querySelector('.message').textContent = "Correct Number!"
 // Do the same thing for secret number and score:
 // label-score - secret number
 // score
-document.querySelector('.number').textContent= 13;
-document.querySelector('.score').textContent= 10;
+// document.querySelector('.number').textContent= 13;
+// document.querySelector('.score').textContent= 10;
 
 // // Select the block, get the value entered. Keep in mind "guess" is an input field.
 // console.log(document.querySelector('.guess').value);
 
-// Manually override too, but instead of user adding
-document.querySelector('.guess').value = 23;
+// // Manually override too, but instead of user adding
+// document.querySelector('.guess').value = 23;
 
-// See it's now permanently 23 due to the permanent set
-console.log(document.querySelector('.guess').value);
+// // See it's now permanently 23 due to the permanent set
+// console.log(document.querySelector('.guess').value);
 
 
 // -------------- LECTURE #2 - HANDLE CLICK EVENTS --------------
@@ -40,10 +40,72 @@ console.log(document.querySelector('.guess').value);
 // addEventListener() is one of the most popular methods
 // Listen for a click.
 
+    // // Show "Correct Number!"
+    // document.querySelector('.message').textContent = "Correct Number!";
+
+
+// Create new number - between 0 and 20:
+const secretNumber = Math.trunc(Math.random()*20)+1;
+
+// Show on the webpage:
+document.querySelector('.number').textContent = secretNumber;
+
+
+// Store starting original score
+// We could use DOM to read and just update the score from there.
+// Not a good practice, should store this somewhere.
+let score = 20;
+
+
 // addEventListener(action, function pass)
+// This function is called here, and only exists once in this script scope.
+// We did not define the function anywhere else.
 document.querySelector('.check').addEventListener('click', function() {
-    console.log(document.querySelector('.guess').value);
+    // Function will receive and console log the guess.
+    
+
+    // Save to a variable
+    const guess = Number(document.querySelector('.guess').value);
+
+    // Check type
+    console.log(typeof guess);
+
+    // Game Logic Evaluation:
+    if (!guess){
+        document.querySelector('.message').textContent = "Please give a valid whole number!😎";
+    } else if (guess === secretNumber) {
+        document.querySelector('.message').textContent = "Great job you guessed the right number 🎊!";
+        //Update score and update on the page:
+        score = score + 1;
+    } else if (guess > secretNumber) {
+        if (score > 1) {
+            document.querySelector('.message').textContent = "Too high!";
+            score--;
+            document.querySelector('.score').textContent = score;
+            console.log(score);
+        } else {
+            document.querySelector('.message').textContent = "You lose the game! :(";
+            document.querySelector('.score').textContent = 0;
+            console.log(score);
+        }
+    } else if (guess < secretNumber){
+        if (score > 1) {
+            document.querySelector('.message').textContent = "Too low!";
+            score--;
+            document.querySelector('.score').textContent = score;
+            console.log(score);
+        } else {
+            document.querySelector('.message').textContent = "You lose the game! :(";
+            document.querySelector('.score').textContent = 0;
+            console.log(score);
+        }
+    }
+
+    // // Show score backend and display the new score.
+    // console.log(score);
+    // document.querySelector('.score').textContent = score;
+    
 });
 
-// Define a function when a click happens. Event Handler.
+// More DOM manipulation.
 
