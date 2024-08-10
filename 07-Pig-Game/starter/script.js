a76980d04a6787c0a468d00f671a08f80caccd8c
TangoMango223 <tang.c.christine@gmail.com>
2024-08-10 16:18:36 -0400

    added more dice code

--- 07-Pig-Game/starter/script.js
+++ 07-Pig-Game/starter/script.js
@@ -1,4 +1,23 @@
 // Implement Strict Mode for JS Coding
 'use strict';
 
+// Starter Code:
+// Recall, selector for unique ID is # hashtag
+const score0El = document.querySelector('#score--0');
+
+// Another way. Slightly faster:
+const score1El = document.getElementById('score--1'); // Works the same, but no hashtag
+
+// Dice conditions - we will hide later
+const diceEl = document.querySelector('.dice');
+
+// Set context to begin at zero
+// This will impact the webpage immediately:
+score0El.textContent = 0;
+score1El.textContent = 0;
+
+// Hide the dice:
+//
+diceEl.classList.add('hidden');
+
 
