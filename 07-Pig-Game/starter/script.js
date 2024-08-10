f6e2c7460b764af4a88db1e1684ea903c0edba80
TangoMango223 <tang.c.christine@gmail.com>
2024-08-10 18:49:24 -0400

    Finished pig game

--- 07-Pig-Game/starter/script.js
+++ 07-Pig-Game/starter/script.js
@@ -6,7 +6,7 @@
 
 // Define some objects:
 const diceEl = document.querySelector('.dice');
-const btnNew = document.querySelector('.btn.btn--new');  // react to buttons
+const btnNew = document.querySelector('.btn.btn--new');  // Reset button for new
 const btnRoll = document.querySelector('.btn.btn--roll');
 const btnHold = document.querySelector('.btn.btn--hold');
 
@@ -18,7 +18,8 @@ const current1El = document.querySelector('#current--1'); // Player 1 display sc
 const score0El = document.querySelector('#score--0');
 const score1El = document.getElementById('score--1'); // Works the same, but no hashtag
 
-
+// Variable to track game status - playing or ending?
+let playing = true;
 
 // Selection Elements for background:
 const player0El = document.querySelector('.player--0');
@@ -29,7 +30,7 @@ let currentScore = 0;
 let activePlayer = 0; // Player 0 always begins, 1 is Player 1.
 
 // Store total scores per player - left = Player 0, right = Player 1;
-const scores = [0,0]; 
+let scores = [0,0]; 
 
 // Starting Conditions:
 // Set context to begin at zero
@@ -61,6 +62,8 @@ const switchPlayers = function(){
 
 // // Rolling dice conditions:
 btnRoll.addEventListener('click', function() {
+
+    if (playing) {
     // Functionality to switch between players:
     
     // 1. Generate random dice roll. Random num from 1 to 6.
@@ -100,40 +103,98 @@ btnRoll.addEventListener('click', function() {
         // // Vice versa infinitely!
         // player0El.classList.toggle('player--active');
         // player1El.classList.toggle('player--active');
-    })
+    }});
 
 // Functionality to hold score and switch player
 btnHold.addEventListener("click", function (){
+    if (playing){
 
     // Internally, let's update the scores array:
     scores[activePlayer] = scores[activePlayer] + currentScore;
     
-    // Visually display the new toal score
+    // Visually display the new total score
     document.getElementById(`score--${activePlayer}`).textContent = scores[activePlayer];
 
 
     // Check if any player's score is or exceeds 100;
     if (scores[activePlayer] >= 20) {
         // Finishes the game:
+        playing = false;
+
         // Remove the active background coloring:
         document.querySelector(`.player--${activePlayer}`).classList.remove('player--active');
 
         // Add the winner coloring
         document.querySelector(`.player--${activePlayer}`).classList.add('player--winner');
 
-        // We need to end the game:
-        
-
+        // Get rid of the dice icon:
+        diceEl.classList.add('hidden');
 
     } else { 
         // Activate function to change active player:
         switchPlayers();
-    }
-
+    }}
 });
 
-// Debugging and testing
-console.log(`Active Player is ${activePlayer}!`);
+// Functionality to fully reset the game
+btnNew.addEventListener("click", function(){
+    // Set playing variable back to true, restoring functionality:
+    playing = true
 
+    // Reset internal variables:
+    currentScore = 0;
+    scores = [0,0] 
+
+    // Reset the scores for both players. Loop thru both players.
+    for(let i = 0; i <2; i ++){
+        // Reset the Current Score:
+        document.getElementById(`current--${i}`).textContent = 0;
+
+        // Reset the total score at the top:
+        document.getElementById(`score--${i}`).textContent = 0;
+
+        // Restore the visual appearance to default for both.
+        // Scenario to remove back:
+        document.querySelector(`.player--${i}`).classList.remove('player--winner');
+        document.querySelector(`.player--${i}`).classList.remove('player--active');
+
+    // Restore the visual appearance. Player 0 will always be the active player at the beginning:
+    document.querySelector('.player--0').classList.add('player--active');
+
+    // Set active player back to player 0:
+    activePlayer = 0;
+    }
+});
+
+
+// Jonas had set a function to establish starting conditions...
+
+const init = function(){
+
+    // Reset scores:
+    const scores = [0,0];
+    let currentScore = 0;
+    let activePlayer = 0;
+    let playing = true;
+
+
+    // Text Content:
+    score0El.textContent = 0;
+    score1El.textContent = 0;
+    current0El.textContent = 0;
+    current1El.textContent = 0;
+
+    // Hidden:
+    diceEl.classList.add('hidden');
+    player0El.classList.remove('player--winner');
+    player1El.classList.remove('player--winner');
+    player0El.classList.add('player--active');
+    player1El.classList.remove('player--active');
+
+};
+
+
+// // Debugging and testing
+// console.log(`Active Player is ${activePlayer}!`);
 
 
