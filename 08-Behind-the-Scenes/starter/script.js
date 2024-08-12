0d9ea7415f5e559d4e0eb118246d5a945e7728c6
TangoMango223 <tang.c.christine@gmail.com>
2024-08-11 21:50:33 -0400

    Updated codes.

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -16,27 +16,29 @@ function calcAge(birthYear) {
 
         if (birthYear >= 1981 && birthYear <= 1996){
             const firstName = "Steven" // In this scope, so the variable will supersede the global variable.
-            var millenial = true;
+            var millennial = true;
             const str = `Oh you are a Millennial, ${firstName}`; // This will say Steven
             console.log(str);
 
             function add(a,b){
                 return a+b;
             }
-
             // Variable reassignment - this reassignment will affect the output outside.
             // But if we redefined it as a new variable, it will not affect the console.log(output)
             // Reassignment:
             output = "New output!"
         }
         // This will fail since out of function scope - console.log(str);
-        console.log(millenial);
+        // console.log(millennial);
         // console.log(add(2,3)); // This will fail on strict mode, since functions are now block scoped in ES6 onward
 
         // It was manipulated and redefined inside this child scope.
         console.log(output); 
+        console.log(millennial);
+        
     printAge();
     }
+
     return age;
 }
 
