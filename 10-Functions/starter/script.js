831c294e7c44cd2caebc88adb7d2322cf4a76fbc
Christine Tang <tang.c.christine@gmail.com>
2024-08-15 17:38:55 -0400

    Started Section 10

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -1,1 +1,27 @@
+// Start with Strict
 'use strict';
+
+// Example:
+const flight = "LH234";
+const jonas = {
+    name: 'Jonas Schmedtmann',
+    password: 123241412123,
+}
+
+// Make a check-in function
+const checkIn = function(flightNum, passenger){
+
+    // Change the Flight Number, not a good practice:
+    flightNum = "LH999";
+    passenger.name = "Mr. " + passenger.name;
+
+    // Check:
+    if (passenger.password === 123241412123) {
+        alert('Check In!');
+    } else {
+        alert(`Wrong passport!`);
+    };
+
+}
+
+checkIn(flight, jonas);
